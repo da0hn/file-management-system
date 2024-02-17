98109d2d4cdc77da67d13a461cf3e3de8d0959cf
Gabriel Honda <gabrielhondacba@gmail.com>
2024-02-17 08:20:04 -0400

    refactor: utiliza `UserRepository` ao invés de `UserRepositoryAdapter`

--- src/main/kotlin/dev/da0hn/file/management/system/application/configuration/security/adapter/UserDetailsServiceAdapter.kt
+++ src/main/kotlin/dev/da0hn/file/management/system/application/configuration/security/adapter/UserDetailsServiceAdapter.kt
@@ -1,9 +1,9 @@
 package dev.da0hn.file.management.system.application.configuration.security.adapter
 
 import dev.da0hn.file.management.system.core.annotation.Adapter
+import dev.da0hn.file.management.system.core.domain.User
 import dev.da0hn.file.management.system.core.extensions.orThrowDataNotFound
-import dev.da0hn.file.management.system.data.db.entities.UserEntity
-import dev.da0hn.file.management.system.data.db.repositories.UserJpaRepository
+import dev.da0hn.file.management.system.core.ports.spi.UserRepository
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -11,16 +11,16 @@ import java.io.Serial
 
 @Adapter
 class UserDetailsServiceAdapter(
-  private val userJpaRepository: UserJpaRepository,
+  private val userRepository: UserRepository,
 ) : UserDetailsService {
   override fun loadUserByUsername(maybeUsername: String?): UserDetails {
     return maybeUsername.orThrowDataNotFound { "Username is required" }
-      .let(userJpaRepository::findByUsername)
+      .let(userRepository::findByUsername)
       .orThrowDataNotFound { "User not found" }
       .let(::UserDetailsImpl)
   }
 
-  class UserDetailsImpl(private val entity: UserEntity) : UserDetails {
+  class UserDetailsImpl(private val entity: User) : UserDetails {
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
       return listOf(entity.role).map { r -> GrantedAuthority { r.name } }.toMutableList()
     }

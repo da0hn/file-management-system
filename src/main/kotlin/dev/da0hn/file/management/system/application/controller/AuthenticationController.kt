34fd387e9472d1664fc07bafc1aa661b7a38d70d
Gabriel Honda <gabrielhondacba@gmail.com>
2024-03-31 11:13:25 -0400

    feat: adiciona log no `AuthenticationController`

--- src/main/kotlin/dev/da0hn/file/management/system/application/controller/AuthenticationController.kt
+++ src/main/kotlin/dev/da0hn/file/management/system/application/controller/AuthenticationController.kt
@@ -2,6 +2,7 @@ package dev.da0hn.file.management.system.application.controller
 
 import dev.da0hn.file.management.system.application.controller.dto.LoginRequest
 import dev.da0hn.file.management.system.core.ports.api.UserAuthenticationUseCase
+import mu.KotlinLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,13 +15,17 @@ class AuthenticationController(
   private val userAuthenticationUseCase: UserAuthenticationUseCase,
 ) {
 
+  private val logger = KotlinLogging.logger { }
+
   @PostMapping("/signin")
   fun authenticateUser(@RequestBody loginRequest: LoginRequest): ResponseEntity<UserAuthenticationUseCase.Output> {
+    logger.info { "m=authenticateUser(username=${loginRequest.username})" }
     val output = userAuthenticationUseCase.execute(
       UserAuthenticationUseCase.Input(
         loginRequest.username,
         loginRequest.password
-      ))
+      )
+    )
     return ResponseEntity.ok(output)
   }
 
